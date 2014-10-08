ce3916476a953a6ce5c75c5a25d580011bae0823
atthapon_cha <atthaphon_jar@truecorp.co.th>
2014-10-08 16:46:00 +0700

    fff

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="test_css.css" />
+</head>
+<body>
+<h1>ทดสอบxxxx</h1>
+</body>
+</html>
--- test.php
+++ test.php
@@ -1,2 +1,22 @@
-<?php
-echo 'xxxsssss';
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Brackets</title>
+    <link rel="stylesheet" href="test_css.css" />
+
+</head>
+
+<body>
+    <h1>ทดสอบ211</h1>
+    asdfsad
+</body>
+<script src="js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        alert('zzz');
+    });
+</script>
+
+</html>
